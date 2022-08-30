90cecc8460e0eb70357d6cc0e897df75710174e5
shigeruhanafusa <orange159159@gmail.com>
2022-08-30 23:49:52 +0900

    新規登録時の正規表現実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
   validates :birth_date, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  validates :password, format: {with: VALID_PASSWORD_REGEX}
 
+  validates :last_name, :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :first_name_kana, :last_name_kana, presence:true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
 end
