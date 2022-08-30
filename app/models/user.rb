030712d07ecd5650b68a35ecb1ee816335cb36da
shigeruhanafusa <orange159159@gmail.com>
2022-08-30 23:34:37 +0900

    パスワードの半角英数正規表現追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
 end
