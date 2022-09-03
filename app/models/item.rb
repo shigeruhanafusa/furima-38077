9719d4d8f20d226b83d3f2d475f4851feede3b79
shigeruhanafusa <orange159159@gmail.com>
2022-09-03 22:41:49 +0900

    Active_hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_day
+
+  validates :area, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :category, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_charge, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_day, numericality: { other_than: 1 , message: "can't be blank" }
+
   belongs_to :user
   has_one_attached :image
 end
