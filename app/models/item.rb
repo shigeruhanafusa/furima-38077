44ffda7deeb16b2a1e4be990208d19023896f230
shigeruhanafusa <orange159159@gmail.com>
2022-09-04 14:33:24 +0900

    validatesの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :shipping_day
 
+  validates :product, presence: true
+  validates :product_description, presence: true
+  validates :price, presence: true, numericality:( greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 )  ,format: { with: /^[0-9]+$/ }
+
   validates :area, numericality: { other_than: 1 , message: "can't be blank" }
   validates :category, numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition, numericality: { other_than: 1 , message: "can't be blank" }
