4eaefed377e85edfa2e5404f3383d0c5c03b21a4
shigeruhanafusa <orange159159@gmail.com>
2022-09-08 14:51:35 +0900

    FactoryBotの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    product               { Faker::Commerce.product_name }
+    product_description   { Faker::Lorem.characters(number: 40, min_alpha: 1, min_numeric: 1) }
+    price                 { Faker::Number.between(from: 300, to: 9999999) }
+    area_id               { Area.find :all, '---' }
+    category_id           { Category.find :all, '---' }
+    condition_id          { Condition.find :all, '---' }
+    shipping_charge_id    { ShippingCharuge.find :all, '---' }
+    shipping_day_id       { ShippingDay.find :all, '---' }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
