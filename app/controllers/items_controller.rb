ca326f129cf92ce1387241389a0b5003ae860ac8
shigeruhanafusa <orange159159@gmail.com>
2022-09-12 17:53:24 +0900

    レビュー箇所の修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit ,:update ,:move_to_index]
   before_action :move_to_index, except: [:index, :show, :new, :create]
-  before_action :set_item, only: [:show, :edit ,:update]
-
+  
   def index
     @items = Item.all.order('created_at ASC')
   end
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
   end
 end
