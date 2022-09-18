df160610ff3c11f07c8c41055a81d553ba5c679e
shigeruhanafusa <orange159159@gmail.com>
2022-09-18 17:28:58 +0900

    shippingsマイグレーションファイルの記入

--- db/migrate/20220918082134_create_shippings.rb
+++ db/migrate/20220918082134_create_shippings.rb
@@ -1,7 +1,13 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
-      
+      t.string     :postal_code,             null: false
+      t.integer    :area_id,                 null: false
+      t.string     :municpality,             null: false
+      t.string     :address1,                null: false
+      t.string     :address2
+      t.string     :phone,                   null: false
+      t.references :purchase_shipping,       null: false, foreign_key: true
       t.timestamps
     end
   end
