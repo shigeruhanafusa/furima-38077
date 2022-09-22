72ae0f9255aa1be8c9af4e447a125e1ae362cc76
shigeruhanafusa <orange159159@gmail.com>
2022-09-22 21:56:46 +0900

    購入ページとエラー表示実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,26 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_shipping = PurchaseShipping.new
+  end
+
+  def create
+    @purchase_shipping = PurchaseShipping.new(pruchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def pruchase_params
+    params.require(:purchase_shipping).permit(:postal_code, :area_id, :municpality, :address1, :address2, :phone).merge(user_id: current_user.id, item_id:params[:item_id])
+  end
+
 end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -5,12 +5,16 @@ class PurchaseShipping
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :user_id
+    validates :item_id
+    validates :municpality
+    validates :address1
     validates :phone, format: {with: /\d{10,11}/, message: "is invalid. Include hyphen(-)"}
   end
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, area_id: area_id, municpality: municpality, address1: address1, address2: address2, purchase_id: purchase_id)
+    Shipping.create(postal_code: postal_code, area_id: area_id, municpality: municpality, address1: address1, address2: address2, phone: phone, purchase_id: purchase.id)
   end
+  
 end
