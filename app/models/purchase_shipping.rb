76a02956b6893f7d2e35149fc11175122cdcc458
shigeruhanafusa <orange159159@gmail.com>
2022-10-15 17:38:18 +0900

    エラーメッセージを英語から日本語に変更

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,15 +3,15 @@ class PurchaseShipping
   attr_accessor :postal_code, :area_id, :municpality, :address1, :address2, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'に(-)を入れてください' }
     validates :token
     validates :user_id
     validates :item_id
     validates :municpality
     validates :address1
-    validates :phone, format: { with: /\d{10,11}/, message: 'is invalid. Include hyphen(-)' }
+    validates :phone, format: { with: /\d{10,11}/, message: 'に(-)を入れてください' }
   end
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "を選択してください" }
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
